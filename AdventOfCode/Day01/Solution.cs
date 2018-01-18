c8e4a3119d5740d7e96406722848178a834162d4
yog <jiri.petrzilka@gmail.com>
2018-01-18 18:39:38 +0100

    Initial commit; gitignore

---
+++ AdventOfCode/Day01/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Day01
+{
+    public class Solution
+    {
+        public int GetResult(string input)
+        {
+
+        }
+    }
+}
---
+++ AdventOfCodeTests/Day01/Day01Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using System.Text;
+using AdventOfCode.Day01;
+
+namespace AdventOfCodeTests.Day01
+{
+    public class Day01Test
+    {
+        [Theory]
+        [InlineData("1122",3)]
+        [InlineData("1111",4)]
+        [InlineData("1234",0)]
+        [InlineData("91212129",9)]
+        public void TestFirstPart(string input, int output)
+        {
+            var solution = new Solution();
+            Assert.Equal(output, solution.GetResult(input));
+        }
+    }
+}
