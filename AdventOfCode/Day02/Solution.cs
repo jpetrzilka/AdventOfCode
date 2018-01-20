3832083d45459262413d0f56349a19d915848b79
yog <jiri.petrzilka@gmail.com>
2018-01-21 00:53:11 +0100

    Solution part 1 for day 3

--- AdventOfCode/Day02/Solution.cs
+++ AdventOfCode/Day02/Solution.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode.Day02
 {
-    class Solution
+    public class Solution
     {
+        public long GetResult1(string input)
+        {
+            return input
+                .Trim()
+                .Split(
+                    new string[] { Environment.NewLine },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .Select(line => line
+                    .Trim()
+                    .Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(number => long.Parse(number)))
+                .Sum(numbers => numbers.Max() - numbers.Min());
+        }
     }
 }
--- AdventOfCodeTests/Day02/Day02Test.cs
+++ AdventOfCodeTests/Day02/Day02Test.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using AdventOfCode.Day02;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace AdventOfCodeTests.Day02
 {
-    class Day02Test
+    public class Day02Test
     {
+        [Fact]
+        public void TestFirstPart()
+        {
+            string input = 
+                @"5 1 9 5
+                7 5 3
+                2 4 6 8";
+
+            var sut = new Solution();
+
+            Assert.Equal(18, sut.GetResult1(input));
+        }
     }
 }
