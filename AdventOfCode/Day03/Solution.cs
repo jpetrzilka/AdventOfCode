ef10126429e0b5b56bd62f63ca60f449487cc972
yog <jiri.petrzilka@gmail.com>
2018-01-27 22:45:37 +0100

    Day 3 - solved part 1

---
+++ AdventOfCode/Day03/Solution.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Day03
+{
+    public class Solution
+    {
+        public long GetResult1(int input)
+        {
+            (int x, int y) = GetNextPosition(input);
+            return Math.Abs(x) + Math.Abs(y);
+        }
+
+        (int x, int y) GetNextPosition(int targetPosition)
+        {
+            int x = 0, y = 0, stepSize = 1, position = 1;
+            Direction currentDirection = Direction.Right;
+
+            while (position <= targetPosition)
+            {
+                for (int i = 0; i < stepSize; i++)
+                {
+                    if (position == targetPosition)
+                        return (x, y);
+
+                    (x, y) = MoveInDirection(x, y, currentDirection);
+                    position++;
+                }
+                currentDirection = (Direction)(((int)currentDirection + 1) % 4);
+
+                for (int i = 0; i < stepSize; i++)
+                {
+                    if (position == targetPosition)
+                        return (x, y);
+
+                    (x, y) = MoveInDirection(x, y, currentDirection);
+                    position++;
+                }
+                currentDirection = (Direction)(((int)currentDirection + 1) % 4);
+
+                stepSize++;
+            }
+
+            return (x, y);
+        }
+
+        (int x, int y) MoveInDirection(int currentX, int currentY, Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.Right:
+                    return (currentX + 1, currentY);
+                case Direction.Up:
+                    return (currentX, currentY + 1);
+                case Direction.Left:
+                    return (currentX - 1, currentY);
+                case Direction.Down:
+                    return (currentX, currentY - 1);
+                default:
+                    throw new NotSupportedException(direction.ToString());
+            }
+        }
+
+
+        public long GetResult2(string input) => throw new NotImplementedException();
+
+        enum Direction
+        {
+            Right = 0,
+            Up = 1,
+            Left = 2,
+            Down = 3
+        }
+    }
+}
---
+++ AdventOfCodeTests/Day03/Day03Test.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode.Day03;
+using Xunit;
+
+namespace AdventOfCodeTests.Day03
+{
+    public class Day03Test
+    {
+        [Theory]
+        [InlineData(1, 0)]
+        [InlineData(12, 3)]
+        [InlineData(23, 2)]
+        [InlineData(1024, 31)]
+        public void TestFirstPart(int input, int expectedOutput)
+        {
+            var sut = new Solution();
+            Assert.Equal(expectedOutput, sut.GetResult1(input));
+        }
+
+        [Fact]
+        public void TestSecondPart()
+        {
+            string input =
+                @"5 9 2 8
+                9 4 7 3
+                3 8 6 5";
+
+            var sut = new Solution();
+
+            Assert.Equal(9, sut.GetResult2(input));
+        }
+    }
+}
